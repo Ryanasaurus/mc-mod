23e23a3abb40822389c21c49a2e9b28637f7cd23
Ryanasaurus <ryanasaurus97@gmail.com>
2020-05-13 14:49:46 +1200

    Added Client/Server Proxy, updated mod data

---
+++ src/main/java/com/ryanasaurus/ryanmod/setup/IProxy.java
@@ -0,0 +1,8 @@
+package com.ryanasaurus.ryanmod.setup;
+
+import net.minecraft.world.World;
+
+public interface IProxy {
+
+    World getClientWorld();
+}
---
+++ src/main/java/com/ryanasaurus/ryanmod/setup/ServerProxy.java
@@ -0,0 +1,10 @@
+package com.ryanasaurus.ryanmod.setup;
+
+import net.minecraft.world.World;
+
+public class ServerProxy implements IProxy {
+    @Override
+    public World getClientWorld() {
+        throw new IllegalStateException("Only run this code on the client");
+    }
+}
